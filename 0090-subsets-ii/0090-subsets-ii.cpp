17c66fe3ef28314bb35be0f0bab7fc1ce40ae24d
sujalraikar2004 <156591292+sujalraikar2004@users.noreply.github.com>
2026-08-13 17:37:38 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -3,21 +3,26 @@ public:
 void f(int index,vector<int>&nums,vector<int>&current,vector<vector<int>>&ans){
     if(index==nums.size()){
         ans.push_back(current);
-        return ;
+        return;
     }
     current.push_back(nums[index]);
     f(index+1,nums,current,ans);
     current.pop_back();
-    f(index+1,nums,current,ans);
+    int next=index+1;
+    while(next<nums.size() && nums[next]==nums[index]){
+        next++;
+    }
+    f(next,nums,current,ans);
+
+    
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>current;
         sort(nums.begin(),nums.end());
         f(0,nums,current,ans);
-        set<vector<int>>st(ans.begin(),ans.end());
-        vector<vector<int>>res(st.begin(),st.end());
-        return res;
+        
+        return ans;
 
     }
 };
