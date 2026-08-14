a0a6466bc2c25dbda5a9be19e3ca1c41683d9120
sujalraikar2004 <156591292+sujalraikar2004@users.noreply.github.com>
2026-08-14 21:26:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
+int fib(int n,vector<int>&dp){
+    if(n<=2)return n;
+    if(dp[n]!=-1)return dp[n];
+    return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+}
     int climbStairs(int n) {
-        if(n<=1)return 1;
-        int prev=1;
-        int prev2=2;
-        for(int i=3;i<=n;i++){
-            int curr=prev+prev2;
-            prev=prev2;
-            prev2=curr;
-
-        }
-        return prev2;
-
+        vector<int>dp(n+1,-1);
+        return fib(n,dp);
     }
 };
